0ab6b33024f7ba7a9b8ed6e3fb37a341035c5195
Mihai Dinculescu <mihai.dinculescu@outlook.com>
2021-01-15 11:27:39 +0000

    Library updates

--- msfs-2020-gps-link/src/components/App.test.tsx
+++ msfs-2020-gps-link/src/components/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-test('renders learn react link', () => {
+test('renders the title', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/MSFS 2020 GPS Link/i);
     expect(linkElement).toBeInTheDocument();
 });

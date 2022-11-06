17970862dae7590f8b63e9917142f800511213c2
Mihai Dinculescu <mihai.dinculescu@outlook.com>
2022-11-06 11:47:48 +0000

    Improve the new version notification

--- www/src/components/Version.tsx
+++ www/src/components/Version.tsx
@@ -1,7 +1,7 @@
 import { Alert } from '@mui/material';
 import React from 'react';
 
-const PACKAGE_VERSION = process.env.REACT_APP_VERSION;
+const PACKAGE_VERSION = process.env.REACT_APP_VERSION?.trim();
 const LATEST_VERSION_URL = 'https://raw.githubusercontent.com/mihai-dinculescu/msfs-2020-gps-link/main/version.txt';
 
 export const Version: React.FC = () => {
@@ -11,18 +11,21 @@ export const Version: React.FC = () => {
         const fn = async () => {
             const req = await fetch(LATEST_VERSION_URL);
             const value = await req.text();
-            setLatestVersion(value);
+            setLatestVersion(value.trim());
         };
 
         fn();
     }, []);
 
-    return latestVersion ? (
-        PACKAGE_VERSION?.trim() === latestVersion?.trim() ? (
+    return PACKAGE_VERSION && latestVersion ? (
+        PACKAGE_VERSION >= latestVersion ? (
             <Alert severity="info">Version {PACKAGE_VERSION}</Alert>
         ) : (
             <Alert severity="warning">
-                There is a new version available. Download it from:
+                You are using version {PACKAGE_VERSION}.
+                <br />
+                <br />
+                {latestVersion} is available. You can download it from:
                 <br />
                 https://github.com/mihai-dinculescu/msfs-2020-gps-link/releases
             </Alert>

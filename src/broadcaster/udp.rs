a055825326bd8e50f391432f67d47bbf1fa40831
Carthorse <john.payne.ak@gmail.com>
2023-03-12 15:06:10 -0800

    Update udp.rs
    convert radians to degress for ground track.

--- src/broadcaster/udp.rs
+++ src/broadcaster/udp.rs
@@ -45,7 +45,7 @@ impl Udp {
 impl BroadcasterExt for Udp {
     #[instrument(name = "Udp::send", skip(self, data))]
     fn send(&mut self, data: GpsData) -> Result<(), io::Error> {
-        let track = data.gps_ground_magnetic_track - data.gps_magnetic_variation;
+        let track = data.gps_ground_magnetic_track * .0174533; //convert radians to degrees.
 
         self.socket
             .send_to(
